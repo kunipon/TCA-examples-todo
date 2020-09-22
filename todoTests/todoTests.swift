cecd7d7dbef4114cd3f39381b95e9ca604cd1f0b
kunipon <kunikunipon.0328@gmail.com>
2020-09-23 00:18:35 +0900

    1秒以内にTODO完了を２回タップしたときのテスト追加

--- todoTests/todoTests.swift
+++ todoTests/todoTests.swift
@@ -86,11 +86,51 @@ class todoTests: XCTestCase {
             .receive(.todoDelayCompleted) {
                 $0.todos.swapAt(0, 1)
                 // $0.todos.swapAt(0, 1)↓と同じ意味
-//                $0.todos = [
-//                  $0.todos[1],
-//                  $0.todos[0],
-//                ]
+                // -----------------
+                // $0.todos = [
+                //   $0.todos[1],
+                //   $0.todos[0],
+                // ]
             }
+        )
+    }
+    
+    func testTodoSorting_Cancellation() {
+        let store = TestStore(
+            initialState: AppState(
+                todos: [
+                    Todo(
+                        id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                        description: "Milk",
+                        isComplete: false
+                    ),
+                    Todo(
+                        id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                        description: "Eggs",
+                        isComplete: false
+                    )
+                ]
+            ),
+            reducer: appReducer,
+            environment: AppEnvironment(
+                uuid: { fatalError("unimplemented") }
+            )
+        )
+        
+        store.assert(
+            .send(.todo(index: 0, action: .checkboxTapped)) {
+                $0.todos[0].isComplete = true
+            },
+            .do {
+                _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 0.5)
+            },
+            .send(.todo(index: 0, action: .checkboxTapped)) {
+                $0.todos[0].isComplete = false
+            },
+            .do {
+                _ = XCTWaiter.wait(for: [self.expectation(description: "wait")], timeout: 1)
+            },
+            .receive(.todoDelayCompleted)
         )
     }
 }
