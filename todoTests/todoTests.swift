d91e0a1d4c7dfe11b5ede1e97a6b265572d75657
kunipon <kunikunipon.0328@gmail.com>
2020-09-22 22:46:43 +0900

    TODO完了時のソートに関するテスト追加

--- todoTests/todoTests.swift
+++ todoTests/todoTests.swift
@@ -15,7 +15,9 @@ class todoTests: XCTestCase {
                 ]
             ),
             reducer: appReducer,
-            environment: AppEnvironment(uuid: UUID.init)
+            environment: AppEnvironment(
+                uuid: { fatalError("unimplemented") }
+            )
         )
         
         store.assert(
@@ -43,6 +45,40 @@ class todoTests: XCTestCase {
                         isComplete: false
                     )
                 ]
+            }
+        )
+    }
+    
+    func testTodoSorting() {
+        let store = TestStore(
+            initialState: AppState(
+                todos: [
+                    Todo(
+                        id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                        description: "Milk",
+                        isComplete: false
+                    ),
+                    Todo(
+                        id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!,
+                        description: "Eggs",
+                        isComplete: false
+                    )
+                ]
+            ),
+            reducer: appReducer,
+            environment: AppEnvironment(
+                uuid: { fatalError("unimplemented") }
+            )
+        )
+        
+        store.assert(
+            .send(.todo(index: 0, action: .checkboxTapped)) {
+                $0.todos[0].isComplete = true
+                $0.todos.swapAt(0, 1)
+//                $0.todos = [
+//                  $0.todos[1],
+//                  $0.todos[0],
+//                ]
             }
         )
     }
