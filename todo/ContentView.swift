e35e67d6197891ae5bb585649309c893799b3501
kunipon <kunikunipon.0328@gmail.com>
2020-09-22 14:41:26 +0900

    composable-architectureを使う準備

--- todo/ContentView.swift
+++ todo/ContentView.swift
@@ -1,13 +1,41 @@
+import ComposableArchitecture
 import SwiftUI
 
+struct AppState {
+}
+
+enum AppAction {
+}
+
+struct AppEnvironment {
+}
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment> { state, action, environment in
+    switch action {
+    }
+}
+
 struct ContentView: View {
+    let store: Store<AppState, AppAction>
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                Text("Hello, World!")
+            }
+            .navigationBarTitle("Todos")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            store: Store(
+                initialState: AppState(),
+                reducer: appReducer,
+                environment: AppEnvironment()
+            )
+        )
     }
 }
